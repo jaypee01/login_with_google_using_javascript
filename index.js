7d3bda8f48858cb66b5b47a2e31b30899e28e462
AzureAD\JyotiParkash <jparkash@snapfinancebpo.com>
2023-11-26 13:56:55 +0530

    code refactor

--- index.js
+++ index.js
@@ -13,10 +13,23 @@ window.onload = function () {
       callback: handleCredentialResponse
     });
     google.accounts.id.renderButton(
-      document.getElementById("buttonDiv"),
+      $( ".myClass" ),
       { theme: "outline", size: "large" }  // customization attributes
     );
     google.accounts.id.prompt(); // also display the One Tap dialog
+  }
+
+  function decodeJwtResponse(token) {
+    var base64Url = token.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
   }
 
 function signOut() {
