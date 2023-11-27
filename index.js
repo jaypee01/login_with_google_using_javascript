a182fa6b09eb416f31404f1d41adb581abe336df
AzureAD\JyotiParkash <jparkash@snapfinancebpo.com>
2023-11-27 08:07:34 +0530

    code refactor

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-// function onSignIn(googleUser) {
-//     var profile = googleUser.getBasicProfile();
-//     $("#name").text(profile.getName());
-//     $("#email").text(profile.getEmail());
-//     $("#image").attr('src', profile.getImageUrl());
-//     $(".data").css("display", "block");
-//     $(".g-signin2").css("display", "none");
-// }
-
-// window.onload = function () {
-//     google.accounts.id.initialize({
-//         client_id: "879873924784-cfi7nis3k852gh17i3ma51ni6ge60ogf.apps.googleusercontent.com",
-//         callback: handleCredentialResponse
-//     });
-//     google.accounts.id.renderButton(
-//         document.getElementById("buttonDiv"),
-//         { theme: "outline", size: "large" }  // customization attributes
-//     );
-//     google.accounts.id.prompt(); // also display the One Tap dialog
-// }
+window.onload = function () {
+    google.accounts.id.initialize({
+        client_id: "879873924784-cfi7nis3k852gh17i3ma51ni6ge60ogf.apps.googleusercontent.com",
+        callback: handleCredentialResponse
+    });
+    google.accounts.id.renderButton(
+        document.getElementById("buttonDiv"),
+        { theme: "outline", size: "large" }  // customization attributes
+    );
+    google.accounts.id.prompt(); // also display the One Tap dialog
+}
 
 function decodeJwtResponse(token) {
     var base64Url = token.split(".")[1];
@@ -33,24 +24,21 @@ function decodeJwtResponse(token) {
     return JSON.parse(jsonPayload);
 }
 
-function init() {
-    gapi.load('auth2', function() {
-        gapi.auth2.init({
-            client_id: '879873924784-cfi7nis3k852gh17i3ma51ni6ge60ogf.apps.googleusercontent.com'
-            // other configuration options if needed
-        });
-      });
-}
+// function init() {
+//     gapi.load('auth2', function() {
+//         gapi.auth2.init();
+//       });
+// }
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        alert("you have been signedout successfully");
-        console.log('User signed out.');
-        $(".g_id_signin").css("display", "block");
-        $(".data").css("display", "none");
-    });
-}
+// function signOut() {
+//     var auth2 = gapi.auth2.getAuthInstance();
+//     auth2.signOut().then(function () {
+//         alert("you have been signedout successfully");
+//         console.log('User signed out.');
+//         $(".g_id_signin").css("display", "block");
+//         $(".data").css("display", "none");
+//     });
+// }
 
 function handleCredentialResponse(response) {
 
